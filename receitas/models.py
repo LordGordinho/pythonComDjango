f334df3c8424e33ef95c81ecf24bae85d14b5146
Jorge Felipe <jfss1@ifal.edu.br>
2021-03-09 06:51:42 -0300

    Quase finalizado

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome_das_receitas = models.CharField(max_length=100)
+    modo_de_preparo = models.TextField(max_length=300)
+    ingredientes = models.TextField()
+    tempo_de_preparo = models.CharField(max_length=100)
+    redimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    criado_em = models.DateTimeField(default=datetime.now, blank=True)
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
 
+    dados = [("0", "Sorvete de cocô"), ("1" ,"Pudim de Barbeiro"), ("2", "Bolo de barata")]
+
+    receitas = {
+        "nome_das_receitas" : dados
+    }
+
+    return render(request, "index.html", receitas)
+
+def receita(request):
+    return  render(request, "receita.html")
